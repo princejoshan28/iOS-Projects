2c775d084e25dba56fc057a2fc805c44388ca106
Prince S <princes@Satishs-Mac-mini.local>
2019-01-22 18:21:16 +0530

    initial commit

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Prince S on 22/01/19.
+//  Copyright © 2019 Prince S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("sdfjkghjsdfrg")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
